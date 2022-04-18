01b71a06fa904f3660c58d1025d20f1a065ffce2
Brian Guterl <brianguterl99@gmail.com>
2022-04-18 19:39:40 -0400

    updated sample-fishes complete

--- src/sample-fishes.js
+++ src/sample-fishes.js
@@ -1,74 +1,80 @@
 const fishes = {
   fish1: {
-    name: 'Pacific Halibut',
-    image:
-      'https://media-cdn.greatbritishchefs.com/media/5w3nxt5k/img24855.jpg',
-    desc: 'Everyone’s favorite white fish. We will cut it to the size you need and ship it.',
-    price: 1724,
-    status: 'available',
-  },
-
-  fish2: {
     name: 'Lobster',
-    image: '/images/lobster.jpg',
+    image:
+      'https://www.thespruceeats.com/thmb/-o0W1_Td2SFqBXwfrPuThC-LGkY=/2533x2533/smart/filters:no_upscale()/steamed-lobster-5097205-hero-06ab15ae53234b0ea162b60d54e57856.jpg',
     desc: 'These tender, mouth-watering beauties are a fantastic hit at any dinner party.',
     price: 3200,
     status: 'available',
   },
 
-  fish3: {
+  fish2: {
     name: 'Sea Scallops',
-    image: '/images/scallops.jpg',
+    image:
+      'https://www.thespruceeats.com/thmb/Doak8NADiBD8VDwWs6lReHo3lKY=/3456x2592/smart/filters:no_upscale()/fresh-sea-scallops-154961718-582140443df78cc2e843df74.jpg',
     desc: 'Big, sweet and tender. True dry-pack scallops from the icey waters of Alaska. About 8-10 per pound',
     price: 1684,
     status: 'unavailable',
   },
 
-  fish4: {
+  fish3: {
     name: 'Mahi Mahi',
-    image: '/images/mahi.jpg',
+    image:
+      'https://www.lemonblossoms.com/wp-content/uploads/2020/06/Grilled-Mahi-Mahi-with-Mango-Sauce-S2.jpg',
     desc: 'Lean flesh with a mild, sweet flavor profile, moderately firm texture and large, moist flakes. ',
     price: 1129,
     status: 'available',
   },
 
-  fish5: {
+  fish4: {
     name: 'King Crab',
-    image: '/images/crab.jpg',
+    image:
+      'https://cdn.shopify.com/s/files/1/0478/1008/5020/products/Jumbo-King-Crab-1_600x.jpg?v=1602714721',
     desc: 'Crack these open and enjoy them plain or with one of our cocktail sauces',
     price: 4234,
     status: 'available',
   },
 
-  fish6: {
+  fish5: {
     name: 'Atlantic Salmon',
-    image: '/images/salmon.jpg',
+    image: 'https://www.supremelobster.com/wp-content/uploads/salmon.jpg',
     desc: 'This flaky, oily salmon is truly the king of the sea. Bake it, grill it, broil it...as good as it gets!',
     price: 1453,
     status: 'available',
   },
 
-  fish7: {
+  fish6: {
     name: 'Oysters',
-    image: '/images/oysters.jpg',
+    image: 'https://infatuation.imgix.net/Lshorter_PEARLZ_Oysters_-1.jpg',
     desc: 'A soft plump oyster with a sweet salty flavor and a clean finish.',
     price: 2543,
     status: 'available',
   },
 
-  fish8: {
+  fish7: {
     name: 'Mussels',
-    image: '/images/mussels.jpg',
+    image:
+      'https://www.thespruceeats.com/thmb/kzeGE6It1hxoXgfruugb96xUfws=/3595x2696/smart/filters:no_upscale()/steamed-mussels-white-wine-recipe-995527-hero-01-5c7d663446e0fb00018bd861.jpg',
     desc: 'The best mussels from the Pacific Northwest with a full-flavored and complex taste.',
     price: 425,
     status: 'available',
   },
 
-  fish9: {
+  fish8: {
     name: 'Jumbo Prawns',
-    image: '/images/prawns.jpg',
-    desc: 'With 21-25 two bite prawns in each pound, these sweet morsels are perfect for shish-kabobs.',
+    image:
+      'https://assets.epicurious.com/photos/5846f05703686d890b756b87/16:9/w_1280,c_limit/Jumbo%20Shrimp%20Marsala%20Housewife-Style.jpg',
+    desc: 'With 21-25 two bite prawns in each pound, these sweet morsels are perfect for kebabs.',
     price: 2250,
+    status: 'available',
+  },
+
+  fish9: {
+    name: 'Pacific Halibut',
+    image:
+      'https://i2.wp.com/www.downshiftology.com/wp-content/uploads/2014/10/Baked-Halibut-main-1.jpg',
+    desc: 'Everyone’s favorite white fish. We will cut it to the size you need and ship it.',
+    price: 1724,
     status: 'available',
   },
 };
